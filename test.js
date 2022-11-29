c003bb37c3ac0869f36b91403b740376470b566f
NMK <117380980+BohdanNMK@users.noreply.github.com>
2022-11-29 10:50:21 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,40 +1,4 @@
 
-function calculationPercent(countTotal, countPart) {
-  let totalCalculate = (countPart / countTotal) * 100;
-  return `${totalCalculate}%`
-
-}
-const countTotal = 200;
-const countPart = 50;
-
-console.log(calculationPercent(countTotal, countPart));
-
-
-11 function calculationPov(radius) {
-  return radius * radius;
-}
-
-function calcAreaCircle(radius) {
-  let x = calculationPov(radius);
-
-  return (3.14 * x);
-
-}
-console.log(calcAreaCircle(100));
-let thicknessFirst
-function boxVolume(length, width, height) {
-
-  return length * width * height;
-
-}
-
-function transferVolumeM2(length, width, height) {
-  const thickness = thicknessFirst;
-  const paramBox = boxVolume(length, width, height)
-  return paramBox / thickness;
-}
-thicknessFirst = 6;
-console.log(transferVolumeM2(5, 5, 5))
 const arguments = process.argv
 
 function overAllVolume(volumeOfOne, quantity) {
@@ -70,22 +34,5 @@ function fullParameterReturn(name, volumeOfOne, quantity) {
 }
 
 console.log(fullParameterReturn(arguments[2], arguments[3], arguments[4]))
-V циліндра = π * r2 * h, де π - число Пі(дорівнює 3, 14), r - радіус основи циліндра, h – висота циліндра
-3.14 * r2 * h
 
-const arguments = process.argv
-
-
-function calculationTotalVolume(weightKg, numberPieces) {
-
-
-  return weightKg / numberPieces;
-
-}
-function returnText(name, weightKg, numberPieces) {
-  total = calculationTotalVolume(weightKg, numberPieces);
-  return `Ми отримали ${name}, у кількості  ${weightKg} кілограм, для виготовлення свічок, шо дозволе нам виготовити ${total} штук,для виготовлення однієї свічки нам потрібно ${numberPieces} грамм воску`;
-}
-
-console.log(returnText(arguments[2], arguments[3], arguments[4]))
 
