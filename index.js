6d8fd72aca4e633d04a65e0391f1b129e11ae890
Bohdan Motenko <117380980+BohdanNMK@users.noreply.github.com>
2023-01-06 10:50:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,19 +4,36 @@ const arguments = process.argv
 function showTwoTables(name, count, volumeOfOne) {
   const str = fullParameterReturn(name, count, volumeOfOne);
   const arr = str.split(' ');
-  arr.splice();
+  const nullArr = str.split('');
+  const spaceArr = [];
+  const singArr = [];
+  nullArr.forEach((element, index) => {
+    console.log(element, index)
+  });
+  spaceArr.push(nullArr[2], nullArr[11], nullArr[21], nullArr[25], nullArr[27], nullArr[37], nullArr[41], nullArr[44], nullArr[49], nullArr[55], nullArr[60], nullArr[70], nullArr[74], nullArr[79], nullArr[82], nullArr[92], nullArr[98], nullArr[103], nullArr[113], nullArr[117], nullArr[120], nullArr[129], nullArr[133], nullArr[136], nullArr[151])
+  singArr.push(nullArr[40], nullArr[81], nullArr[95], nullArr[116])
+
+
+
+
 
   const Text = `"Таблиця чисел з калькуляцій додатку:"`;
 
-  const newArr = [parseInt(arr[6].substring(0, arr[6].length - 1)), parseInt(arr[14].substring(0, arr[14].length - 1)), parseInt(arr[19].substring(0, arr[19].length - 1))];
+  let newArr = [];
 
 
-  console.table(arr);
-  console.log(Text);
-  console.table(newArr);
-  return str;
+  const checkIndexes = [6, 14, 19];
+
+  for (let step = 0; step < arr.length; step++) {
+    if (checkIndexes.includes(step)) newArr.push(parseInt(arr[step].substring(0, parseInt(arr[step].length - 1))));
+  }
+
+
+  console.log(nullArr)
+  console.log(singArr)
+  console.log(spaceArr)
+  return newArr;
 }
-
 
 function overAllVolume(volumeOfOne, count) {
 
@@ -50,6 +67,6 @@ function fullParameterReturn(name, count, volumeOfOne) {
   return `${textResult}. Загальний об'єм цієї продукції ${countTotal}, що дозволяє нам її транспортувати ${typeVechicle}`;
 }
 
+console.table(showTwoTables(arguments[2], arguments[3], arguments[4]))
 
-console.log(showTwoTables(arguments[2], arguments[3], arguments[4]))
 
